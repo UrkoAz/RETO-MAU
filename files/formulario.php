f82fa27c5ad31f21f666a1d9df63b2212c4d4bb3
UrkoAz <urko.azkarate@maristak.net>
2023-11-07 09:21:59 +0100

    form
    formulario, carpeta img y logo MAU

---
+++ files/formulario.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<title>Alta de corredor</title>
+<head>
+    <link rel="icon" href="../img/maulogo.jpg" type="image/icon type">
+    <link rel="stylesheet" type="text/css" href="css/estilos2.css">
+    <link rel="stylesheet" type="text/css" href="css/estilotabla.css">
+</head>
+<body>
+    <form method="post" action="alta_corredor.php">
+        Nombre: <input type="text" name="nombre" maxlength="255" required="required"></br>
+        Apellido: <input type="text" name="apellido" maxlength="255" required="required"></br>
+        Contrase&ntilde;a: <input type="text" name="contrasena" maxlength="255" required="required"></br>
+        Huella: <input type="text" name="huella" maxlength="255" required="required"></br>
+        <input type="submit" name="btn_alta" value="Alta">
+    </form>
+</body>
+</html>
