23a0be7773f4ef37c88e4ac98b66f035e787acc6
UrkoAz <urko.azkarate@maristak.net>
2023-11-28 12:01:22 +0100

    comments

--- JS/script.js
+++ JS/script.js
@@ -1,66 +1,69 @@
-let tiempoInicio;
-let tiempoPausado = 0;
-let cronometroActivo = false;
+let tiempoInicio; //Almacena el tiempo cuando se inicia el cronómetro.
+let tiempoPausado = 0; //Almacena el tiempo cuando el cronómetro se para.
+let cronometroActivo = false; //dice si el cronómetro está activo o no.
     function iniciarCronometro() {
-        if (!cronometroActivo) {
-                if (tiempoPausado === 0) {
-                    tiempoInicio = new Date().getTime();
+        if (!cronometroActivo) { //Inicia el cronómetro si no está activo.
+                if (tiempoPausado === 0) { 
+                    tiempoInicio = new Date().getTime(); //Si es la primera vez que se inicia, guarda el tiempo en "tiempoInicio".
                 } else {
-                    tiempoInicio = new Date().getTime() - tiempoPausado;
+                    tiempoInicio = new Date().getTime() - tiempoPausado; //Si se reanuda después de pararlo, ajusta "tiempoInicio" teniendo en cuenta el tiempo pausado.
                     tiempoPausado = 0;
                 }
 
-                cronometroActivo = true;
-                actualizarCronometro();
+                cronometroActivo = true; //Activa el cronómetro.
+                actualizarCronometro(); //llama a la función "actualizarCronometro()"".
         }
     }
     function detenerCronometro() {
-        cronometroActivo = false;
-        tiempoPausado = new Date().getTime() - tiempoInicio;
+        cronometroActivo = false; //Detiene el cronómetro poniendo "cronometroActivo" en false.
+        tiempoPausado = new Date().getTime() - tiempoInicio; //Guarda el tiempo actual en "tiempoPausado" para luego poder reanudar.
     }
-    function reiniciarCronometro() {
+    function reiniciarCronometro() { //Detiene el cronómetro y restablece las variables a sus valores iniciales.
         cronometroActivo = false;
         tiempoPausado = 0;
-        document.getElementById("cronometro").innerHTML = "00:00:00";
+        document.getElementById("cronometro").innerHTML = "00:00:00"; //Actualiza el div con id "cronometro" de "cronometro.php" para que salga: "00:00:00".
     }
     function actualizarCronometro() {
         if (cronometroActivo) {
-            let tiempoActual = new Date().getTime();
+            let tiempoActual = new Date().getTime(); //Si el cronómetro está activo, calcula el tiempo que ha pasado desde el inicio.
             let tiempoTranscurrido = tiempoActual - tiempoInicio;
     
-            let segundosTotales = Math.floor(tiempoTranscurrido / 1000);
-            let horas = Math.floor(segundosTotales / 3600);
+            let segundosTotales = Math.floor(tiempoTranscurrido / 1000); //Convierte el tiempo transcurrido a horas, minutos y segundos (el tiempo está en milisegundos).
+            let horas = Math.floor(segundosTotales / 3600); //Math.floor para que siempre tengan dos dígitos.
             let minutos = Math.floor((segundosTotales % 3600) / 60);
             let segundos = segundosTotales % 60;
     
-            // Agrega un cero delante si son menores a 10
+            //Agrega un cero delante si son menores a 10
             horas = horas < 10 ? "0" + horas : horas;
             minutos = minutos < 10 ? "0" + minutos : minutos;
             segundos = segundos < 10 ? "0" + segundos : segundos;
 
+            //Actualiza el div con id "cronometro" de "cronometro.php" para que salga el tiempo actualizado.
             document.getElementById("cronometro").innerHTML = `${horas}:${minutos}:${segundos}`;
-
+            
+            //Programa una llamada después de 1s.
             setTimeout(actualizarCronometro, 1000);
         }
     }
 
     function enviarCronometro() {
-        var valorCronometro = document.getElementById("cronometro").innerHTML;
+        var valorCronometro = document.getElementById("cronometro").innerHTML; //Coge el valor del div con id "cronometro" de "cronometro.php".
         /*alert(valorCronometro);*/
-        document.getElementById("valorCronometro").value = valorCronometro;
+        document.getElementById("valorCronometro").value = valorCronometro; //Asigna ese valor a "valorCronometro".
     }
 
     //cookies
     function alertCookies() {
-        var userResponse = confirm('Aceptas las cookies?');
+        var userResponse = confirm('Aceptas las cookies?'); //Muestra un cuadro de confirmación al usuario preguntando si acepta las cookies.
         
             if (userResponse) {
-            alert('Has aceptado las cookies!');
+            alert('Has aceptado las cookies!'); //Si el usuario acepta, sale por pantalla un mensaje.
         } else {
-            window.history.back();
+            window.history.back(); //Si el usuario no acepta, vuelve a la página anterior.
         }
     }
 
+//Haces click en los botones y dependiendo de cual pulses llama a una función u otra.
 document.getElementById("Btn_Iniciar").addEventListener("click", iniciarCronometro);
 document.getElementById("Btn_Detener").addEventListener("click", detenerCronometro);
 document.getElementById("Btn_Reiniciar").addEventListener("click", reiniciarCronometro);
